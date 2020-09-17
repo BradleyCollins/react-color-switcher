5c4bddb799714591398820c6bd95e77bcad2669f
Bradley Collins <bradleycollins@Bradleys-MacBook-Pro.local>
2020-09-16 18:30:18 -0700

    Updates

---
+++ precache-manifest.58248ec37649f42a9b5f941b61ba718f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0d2c58131b1c1b861dfc19fa82a5fc",
+    "url": "/react-color-switcher/index.html"
+  },
+  {
+    "revision": "b3b9b8b509fb7efedbb4",
+    "url": "/react-color-switcher/static/css/main.8b5b2d5c.chunk.css"
+  },
+  {
+    "revision": "f7e879595dc3c5bdbccc",
+    "url": "/react-color-switcher/static/js/2.e29a37e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-color-switcher/static/js/2.e29a37e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3b9b8b509fb7efedbb4",
+    "url": "/react-color-switcher/static/js/main.2ba4ef5b.chunk.js"
+  },
+  {
+    "revision": "6f18c0324c649caeb73f",
+    "url": "/react-color-switcher/static/js/runtime-main.615626f3.js"
+  }
+]);
